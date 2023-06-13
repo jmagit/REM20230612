9322752321b1a22dc47aa553ed4d71c234bf5287
Javier <jmartin@grupoloyal.com>
2023-06-13 11:33:59 +0200

    pipes

--- curso/src/app/demos/demos.component.ts
+++ curso/src/app/demos/demos.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NotificationService } from '../common-services';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-demos',
   templateUrl: './demos.component.html',
-  styleUrls: ['./demos.component.css']
+  styleUrls: ['./demos.component.css'],
+  providers: [ NotificationService ]
 })
 export class DemosComponent {
-  constructor(public vm: NotificationService) { }
+  listado: Observable<any>;
+
+  constructor(public vm: NotificationService, http: HttpClient) {
+    this.listado = http.get('http://localhost:4321/api/peliculas')
+   }
 }
