39783c9854edcd585334daadc7f42aae3ce49a4d
Javier <jmartin@grupoloyal.com>
2023-06-13 09:09:37 +0200

    directivas

--- curso/src/lib/my-core/my-core.module.ts
+++ curso/src/lib/my-core/my-core.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PIPES_CADENAS } from './pipes/cadenas.pipe';
 import { PIPES_NUMERICOS } from './pipes/numericos.pipe';
+import { DIRECTIVAS_ATRIBUTO } from './directives/atributos.directive';
+import { UnlessDirective } from './directives/estructurales.directive';
+import { MIS_VALIDADORES } from './directives/validadores/mis-validaciones.directive';
 
 
 
 @NgModule({
   declarations: [
-    PIPES_CADENAS, PIPES_NUMERICOS,
+    PIPES_CADENAS, PIPES_NUMERICOS, MIS_VALIDADORES, DIRECTIVAS_ATRIBUTO, UnlessDirective,
   ],
   exports: [
-    PIPES_CADENAS, PIPES_NUMERICOS,
+    PIPES_CADENAS, PIPES_NUMERICOS, MIS_VALIDADORES, DIRECTIVAS_ATRIBUTO, UnlessDirective,
   ],
   imports: [
     CommonModule
