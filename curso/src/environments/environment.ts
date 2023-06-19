49741fa97fd7b210aee0886e0b754da16d5b1234
Javier <jmartin@grupoloyal.com>
2023-06-19 10:19:16 +0200

    proxy

--- curso/src/environments/environment.ts
+++ curso/src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   ERROR_LEVEL: 99,
   apiURL: 'http://localhost:4321/api/',
+  // apiURL: '/api/',
   securityApiURL: 'http://localhost:4321/',
   wsEndpoint:  'ws://localhost:4321/ws/',
 };
